a0d77ebcc1989a3de441d2c4cae98a6cb0c9e040
Soorajkumar0705 <141757295+Soorajkumar0705@users.noreply.github.com>
2023-12-14 23:44:20 +0530

    Add files via upload
    + initial commit

---
+++ HelperClasses/HelperClasses/Helper/ViewClass.swift
@@ -0,0 +1,10 @@
+//
+//  ViewClass.swift
+//  HelperClasses
+//
+//  Created by Apple on 14/12/23.
+//
+
+import UIKit
+
+
---
+++ HelperClasses/HelperClasses/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelperClasses
+//
+//  Created by Apple on 14/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
